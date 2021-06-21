ba9e11bb229f269941ab59fab8df192474f844a9
Gerardo25Dev <Gerardo25Dev@gmail.com>
2021-06-20 20:26:05 -0400

    mobile design finish

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const navIcon = document.getElementById("nav-icon");
-
-// ? nav list
-const navList = document.getElementById("nav-list");
-
-// ? modal
-const modals = document.getElementById("modals");
-const downCards = document.getElementById("down-cards");
-
-const modalDefault = document.getElementById("modal-default");
-const modalSuccess = document.getElementById("modal-success");
-
 // ? altura del documento
 const body = document.body;
 const html = document.documentElement;
@@ -20,6 +8,72 @@ const height = Math.max(
    html.scrollHeight,
    html.offsetHeight
 );
+
+// ? NavBar
+const navIcon = document.getElementById("nav-icon");
+const navList = document.getElementById("nav-list");
+
+// ? modal
+const modals = document.getElementById("modals");
+const downCards = document.getElementById("down-cards");
+const modalDefault = document.getElementById("modal-default");
+const modalSuccess = document.getElementById("modal-success");
+
+// ? modal card 2
+// const label2 = document.getElementById("label2");
+const modalCard2 = document.getElementById("modal-card-2");
+const radioModal2 = document.getElementById("radioModal2");
+const modalFooter2 = document.getElementById("modal-footer-2");
+
+// ? modal card 3
+// const label3 = document.getElementById("label3");
+const modalCard3 = document.getElementById("modal-card-3");
+const radioModal3 = document.getElementById("radioModal3");
+const modalFooter3 = document.getElementById("modal-footer-3");
+
+
+// *************************************
+// ? ********* Show/Hiden Menu *********
+
+const showMenu = () => {
+   navList.classList.add("nav-list-block");
+   navIcon.classList.add("nav-icon-hidden");
+};
+const hiddenMenu = () => {
+   navList.classList.remove("nav-list-block");
+   navIcon.classList.remove("nav-icon-hidden");
+};
+
+// *************************************
+// ? ********* selected modal *********
+
+const handleModalSelected = (e) => {
+   switch (e.trim()) {
+      case "radioModal2":
+         radioModal2.setAttribute("checked", "");
+         modalCard2.classList.add("modal-card-selected");
+         modalFooter2.classList.remove("modal-footer-none");
+
+         radioModal3.removeAttribute("checked");
+         modalCard3.classList.remove("modal-card-selected");
+         modalFooter3.classList.add("modal-footer-none");
+
+         break;
+      case "radioModal3":
+         radioModal3.setAttribute("checked", "");
+         modalCard3.classList.add("modal-card-selected");
+         modalFooter3.classList.remove("modal-footer-none");
+
+         radioModal2.removeAttribute("checked");
+         modalCard2.classList.remove("modal-card-selected");
+         modalFooter2.classList.add("modal-footer-none");
+
+         break;
+      default:
+         console.log(e);
+         break;
+   }
+};
 
 // *************************************
 // ? ********* loaded the file *********
@@ -34,17 +88,8 @@ document.body.addEventListener("resize", () => {
 });
 
 // *************************************
-// ? ********* Show/Hiden Menu *********
+// ? ******* Show/Hidden NavBar *******
 // *************************************
-
-const showMenu = () => {
-   navList.classList.add("nav-list-block");
-   navIcon.classList.add("nav-icon-hidden");
-};
-const hiddenMenu = () => {
-   navList.classList.remove("nav-list-block");
-   navIcon.classList.remove("nav-icon-hidden");
-};
 
 navIcon.addEventListener("click", (e) => {
    e.stopPropagation();
@@ -81,19 +126,24 @@ modals.addEventListener("click", (e) => {
    }
 });
 
-// *************************************
-// ? ********** modal sucess ***********
-// *************************************
-
 modalDefault.addEventListener("click", (e) => {
    if (e.target.classList.contains("modal-footer__btn")) {
+      // *************************************
+      // ? ******* open modal success ********
       modalDefault.classList.add("modal-default-none");
       modalSuccess.classList.remove("modal-success-none");
+   } else {
+      // *************************************
+      // ? ********* card selected **********
+      if (e.target.classList.contains("modal-checkout__label")) {
+         handleModalSelected(e.target.getAttribute("for"));
+      }
    }
 });
 
+// *************************************
+// ? ****** closed modal success *******
 modalSuccess.addEventListener("click", (e) => {
-   // modal-success__btn
    if (e.target.classList.contains("modal-success__btn")) {
       modalSuccess.classList.add("modal-success-none");
       modals.classList.add("modals-none");
